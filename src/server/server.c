37b27b3bd97cdaaebf2a0d4f72b59457568bda87
whynotmeu <lizazvonovalizo@gmail.com>
2026-01-22 00:06:40 +0300

    1
    Added comments in Russian to explain the server code.

--- src/server/server.c
+++ src/server/server.c
@@ -1,9 +1,9 @@
-#include <fcgi_stdio.h>
+#include <fcgi_stdio.h>   // Подключение станд библиотеки FastCGI
 
-int main(void) {
-    while (FCGI_Accept() >= 0) {
-        printf("Content-type: text/html\r\n\r\n");
-        printf("<html><body><h1>Hello World!</h1></body></html>\n");
-    }
-    return 0;
+int main(void) {    // Главная функция программы
+    while (FCGI_Accept() >= 0) {  // ждёт новый HTTP-запрос
+        printf("Content-type: text/html\r\n\r\n");  // заголовок HTTP, обязательно для браузера
+        printf("<html><body><h1>Hello World!</h1></body></html>\n");  // то, что увидит пользователь
+    }  // цикл while-сервер работает постоянно и не закрывается после одного запроса
+    return 0; // Завершаем программу
 }
