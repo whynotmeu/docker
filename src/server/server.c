7fe3eaea328e6e242f7d54fadda3671e5fd095cb
whynotmeu <lizazvonovalizo@gmail.com>
2026-01-23 00:25:07 +0300

    1

--- src/server/server.c
+++
@@ -1,9 +0,0 @@
-#include <fcgi_stdio.h>   // Подключение станд библиотеки FastCGI
-
-int main(void) {    // Главная функция программы
-    while (FCGI_Accept() >= 0) {  // ждёт новый HTTP-запрос
-        printf("Content-type: text/html\r\n\r\n");  // заголовок HTTP, обязательно для браузера
-        printf("<html><body><h1>Hello World!</h1></body></html>\n");  // то, что увидит пользователь
-    }  // цикл while-сервер работает постоянно и не закрывается после одного запроса
-    return 0; // Завершаем программу
-}
